d5248b11af38ed5a8fffff27c71a862fa4438926
Nathan Dias <nathan.dias@gmail.com>
2021-06-10 13:24:26 -0700

    add nonce to csv import form for security

--- dingsbeer/import_csv.php
+++ dingsbeer/import_csv.php
@@ -27,6 +27,8 @@ Brewery, Beer Name, Series Name, Year, Style, ABV, Format, Total, Appearance, Sm
 
 <!-- Form -->
 <form method='post' action='<?= $_SERVER['REQUEST_URI']; ?>' enctype='multipart/form-data'>
+
+<?php wp_nonce_field( 'dbb_import_csv', '_dbb_nonce' ); ?>
   
 <table style="text-align:left">
 <tbody>
@@ -50,173 +52,208 @@ Brewery, Beer Name, Series Name, Year, Style, ABV, Format, Total, Appearance, Sm
 
 // Import CSV
 
+
+
+
 if(isset($_POST['butimport'])) {
 
-  try {
-   
-    // Undefined | Multiple Files | $_FILES Corruption Attack
-    // If this request falls under any of them, treat it invalid.
-    if (
-        !isset($_FILES['import_file']['error']) ||
-        is_array($_FILES['import_file']['error'])
-    ) {
-        throw new RuntimeException('Invalid parameters.');
-    }
 
-    // Check $_FILES['import_file']['error'] value.
-    switch ($_FILES['import_file']['error']) {
-      case UPLOAD_ERR_OK:
-          break;
-      case UPLOAD_ERR_NO_FILE:
-          throw new RuntimeException('No file sent.');
-      case UPLOAD_ERR_INI_SIZE:
-      case UPLOAD_ERR_FORM_SIZE:
-          throw new RuntimeException('Exceeded filesize limit.');
-      default:
-          throw new RuntimeException('Unknown errors.');
-    }
+  if ( ! isset( $_POST['_dbb_nonce'] ) 
+    || ! wp_verify_nonce( $_POST['_dbb_nonce'], 'dbb_import_csv' ) 
+  ) {
+   print 'Sorry, your nonce did not verify.';
+   exit;
+  } else {
+   // process form data
 
-    // You should also check filesize here.
-    if ($_FILES['import_file']['size'] > 2097152) {
-      throw new RuntimeException('Exceeded filesize limit.');
-    }
-
-    // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
-    // Check MIME Type by yourself.
+    try {
     
-    // File MIME type checking is currently inactive
-    // Okay for now, since only admin users can access this page
-    // would like to fix this eventually.
+      // Undefined | Multiple Files | $_FILES Corruption Attack
+      // If this request falls under any of them, treat it invalid.
+      if (
+          !isset($_FILES['import_file']['error']) ||
+          is_array($_FILES['import_file']['error'])
+      ) {
+          throw new RuntimeException('Invalid parameters.');
+      }
 
+      // Check $_FILES['import_file']['error'] value.
+      switch ($_FILES['import_file']['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Exceeded filesize limit.');
+        default:
+            throw new RuntimeException('Unknown errors.');
+      }
 
-    // $finfo = new finfo(FILEINFO_MIME_TYPE);
-    // if (false === $ext = array_search(
-    //     $finfo->file($_FILES['import_file']['tmp_name']),
-    //     array(
-    //         'csv' => 'text/csv',
-    //     ),
-    //     true
-    // )) {
-    //     throw new RuntimeException('Invalid file format.');
-    // }
+      // You should also check filesize here.
+      if ($_FILES['import_file']['size'] > 2097152) {
+        throw new RuntimeException('Exceeded filesize limit.');
+      }
 
-    echo '<h3 style="color:green">File uploaded successfully</h3>';
-
-    $uploaded_filename = $_FILES['import_file']['name'];
-
-    error_log("DIR: " . __DIR__ );
-    error_log("UPLOAD NAME: " . $uploaded_filename);
-  
-    // File extension
-    $extension = pathinfo($uploaded_filename, PATHINFO_EXTENSION);
-
-    // If file extension is 'csv'
-    if(!empty($uploaded_filename) && $extension == 'csv'){
-
-      $totalInserted = 0;
-
-      $tmp_filename = $_FILES['import_file']['tmp_name'];
-      error_log("TMP NAME: " . $tmp_filename);
-      error_log("DOCUMENT ATTACH: " . print_r($_FILES, true));
-      // Open file in read mode
-
-      $csvFile = fopen($tmp_filename, 'r') or die ("can't open file");
-
-      fgetcsv($csvFile); // Skipping header row
-
-      echo "<table width='100%' border='1' style='border-collapse: collapse'>\n";
-      echo "<tbody>\n";
-
-
-      $rowCount = 0;
-
-      // Read file
-      while(($csvData = fgetcsv($csvFile)) !== FALSE){
-
-        $rowCount++;
-        error_log("attempting insert of row # " . $rowCount);
-
-        
-        if ($_POST['file_encoding'] == 'UTF-8') {
-          $csvData = array_map("utf8_encode", $csvData);
-        } elseif ($_POST['file_encoding'] == 'CP1252') {
-          $csvData = array_map('my_mb_encode', $csvData);
-        } else {
-          throw new RuntimeException ("Invalid file encoding specified: " . $_POST['file_encoding']);
-        }
-
-
-        // Row column length
-        $dataLen = count($csvData);
-
-        // Skip row if length != 15
-        if( !($dataLen == 15) ) {
-          error_log("skipping row # $rowCount; column count != 15");
-          throw new RuntimeException("Invalid file format: rows should have exactly 15 columns");
-          continue;
-        }
-
-        // Assign value to variables
-        list ($brewery, $beer_name, $series_name, $year, $style, $abv, $format, $total,
-          $appearance, $smell, $taste, $mouthfeel, $overall, $review_date, $notes) = array_map('trim', $csvData);
-
-        // remove a percent sign from ABV if it's included
-        $abv = rtrim($abv, "\%");
-
-        // normalize the review date format
-        $review_date = new DateTime($review_date);
-        $review_date = $review_date->format('Y-m-d h:i:s');
+      // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+      // Check MIME Type by yourself.
       
-        $post_id = wp_insert_post(array(
-          'post_title'=> htmlentities($beer_name), 
-          'post_type'=>'dingsbeerblog_beer', 
-          'post_content'=> htmlentities($notes),
-          'post_status' => 'publish',
-          'post_date' => $review_date,
-          'comment_status' => 'closed',
-          'pingback_status' => 'closed',
-        ));
+      // File MIME type checking is currently inactive
+      // Okay for now, since only admin users can access this page
+      // would like to fix this eventually.
 
-        if ($post_id) {
-          error_log("inserted post_id = $post_id");
-        } else {
-          error_log("failed to insert that row");
-        }
+
+      // $finfo = new finfo(FILEINFO_MIME_TYPE);
+      // if (false === $ext = array_search(
+      //     $finfo->file($_FILES['import_file']['tmp_name']),
+      //     array(
+      //         'csv' => 'text/csv',
+      //     ),
+      //     true
+      // )) {
+      //     throw new RuntimeException('Invalid file format.');
+      // }
+
+      echo '<h3 style="color:green">File uploaded successfully</h3>';
+
+      $uploaded_filename = $_FILES['import_file']['name'];
+
+      error_log("DIR: " . __DIR__ );
+      error_log("UPLOAD NAME: " . $uploaded_filename);
+    
+      // File extension
+      $extension = pathinfo($uploaded_filename, PATHINFO_EXTENSION);
+
+      // If file extension is 'csv'
+      if(!empty($uploaded_filename) && $extension == 'csv'){
+
+        $totalInserted = 0;
+
+        $tmp_filename = $_FILES['import_file']['tmp_name'];
+        error_log("TMP NAME: " . $tmp_filename);
+        error_log("DOCUMENT ATTACH: " . print_r($_FILES, true));
+        // Open file in read mode
+
+        $csvFile = fopen($tmp_filename, 'r') or die ("can't open file");
+
+        fgetcsv($csvFile); // Skipping header row
+
+        echo "<table width='100%' border='1' style='border-collapse: collapse'>\n";
+        echo "<tbody>\n";
+
+
+        $rowCount = 0;
+
+        // Read file
+        while(($csvData = fgetcsv($csvFile)) !== FALSE){
+
+          $rowCount++;
+          error_log("attempting insert of row # " . $rowCount);
 
           
-        if ($post_id) {
-            // insert post meta
-            $meta_key_names = array('series_name', 'year', 'abv', 'total',
-                'appearance', 'smell', 'taste', 'mouthfeel', 'overall');
+          if ($_POST['file_encoding'] == 'UTF-8') {
+            $csvData = array_map("utf8_encode", $csvData);
+          } elseif ($_POST['file_encoding'] == 'CP1252') {
+            $csvData = array_map('my_mb_encode', $csvData);
+          } else {
+            throw new RuntimeException ("Invalid file encoding specified: " . $_POST['file_encoding']);
+          }
 
-            foreach ($meta_key_names as $meta_key_name) {
-                add_post_meta($post_id, $meta_key_name, htmlentities(${"$meta_key_name"}));
-            }
 
-            // associate taxonomy terms with the post
-            // wp_create_term creates a new term if it doesn't exist already
+          // Row column length
+          $dataLen = count($csvData);
 
-            wp_set_object_terms($post_id, $format, 'format');
-            wp_set_object_terms($post_id, $brewery, 'brewery');
-            wp_set_object_terms($post_id, $style, 'style');
+          // Skip row if length != 15
+          if( !($dataLen == 15) ) {
+            error_log("skipping row # $rowCount; column count != 15");
+            throw new RuntimeException("Invalid file format: rows should have exactly 15 columns");
+            continue;
+          }
 
+          // Assign value to variables (and remove whitespace)
+          $field_names = ['brewery', 'beer_name', 'series_name', 'year', 'style', 'abv', 'format', 'total',
+            'appearance', 'smell', 'taste', 'mouthfeel', 'overall', 'review_date', 'notes'];
+
+          // read the CSV data into an associative array, field_name => value
+          $i = 0;
+          $data = [];
+          foreach ($field_names as $field_name) {
+            $data[$field_name] = trim($csvData[$i]);
+            error_log("$field_name = " . $csvData[$i]);
+            $i++;
+          }
+
+          // remove a trailing percent sign(s) from ABV if it's included
+          $data['abv'] = rtrim($data['abv'], "\%");
+
+          // normalize the review date format
+          $review_date = $data['review_date'];
+          $review_date = new DateTime($review_date);
+          $data['review_date'] = $review_date->format('Y-m-d h:i:s');
+
+          // encode htmlentities for each key, value
+          foreach ($data as $value) {
+            $value = htmlentities($value);
+          }
+
+          // alias the data so that we can reference $data['brewery'] as $brewery
+          // and changing $brewery will update $data['brewery'];
+          foreach ($field_names as $field_name) {
+            ${$field_name} =& $data[$field_name];
+          }
+
+          $post_id = wp_insert_post(array(
+            'post_title'=> $beer_name, 
+            'post_type'=>'dingsbeerblog_beer', 
+            'post_content'=> $notes,
+            'post_status' => 'publish',
+            'post_date' => $review_date,
+            'comment_status' => 'closed',
+            'pingback_status' => 'closed',
+          ));
+
+          if ($post_id) {
+            error_log("inserted post_id = $post_id");
+          } else {
+            error_log("failed to insert that row");
+          }
+
+            
+          if ($post_id) {
+              // insert post meta
+              $meta_key_names = array('series_name', 'year', 'abv', 'total',
+                  'appearance', 'smell', 'taste', 'mouthfeel', 'overall');
+
+              foreach ($meta_key_names as $meta_key_name) {
+                  add_post_meta($post_id, $meta_key_name, ${"$meta_key_name"});
+              }
+
+              // associate taxonomy terms with the post
+              // wp_create_term creates a new term if it doesn't exist already
+
+              wp_set_object_terms($post_id, $format, 'format');
+              wp_set_object_terms($post_id, $brewery, 'brewery');
+              wp_set_object_terms($post_id, $style, 'style');
+
+          }
+          
+          $totalInserted++;
         }
-        
-        $totalInserted++;
-      }
-    
       
-      echo "<h3 style='color: green;'>Total record Inserted : ".$totalInserted."</h3>";
-    } else {
+        
+        echo "<h3 style='color: green;'>Total record Inserted : ".$totalInserted."</h3>";
+      } else {
 
-      throw new RuntimeException('Invalid File Extension');
+        throw new RuntimeException('Invalid File Extension');
 
+      }
+    } catch (RuntimeException $e) {
+
+        echo "<h3 style='color:red'>" . $e->getMessage() . "</h3>";
+    
     }
-  } catch (RuntimeException $e) {
-
-      echo "<h3 style='color:red'>" . $e->getMessage() . "</h3>";
-  
   }
+
 }
 
 
