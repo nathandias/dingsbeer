871f5c39060c9480e77b198db71082ebe3591bb9
Nathan Dias <nathan.dias@gmail.com>
2021-06-10 13:48:51 -0700

    add Divi child theme with beer review customization

---
+++ Divi-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function divi_child_enqueue_styles() { 
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'divi_child_enqueue_styles' );
