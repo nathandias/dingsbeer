cf1b4cc7ab22a17258490e1c383a9cdc008d5f1e
Nathan Dias <nathan.dias@gmail.com>
2021-06-23 14:42:44 -0700

    minor plugin version increment

--- dingsbeer/plugin.php
+++ dingsbeer/plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ding's Beer Blog - Beer Reviews
  * Plugin URI: https://nathandias.com/dingsbeerreviews
  * Description: Custom post types and utilities for beer reviews for dingsbeerblog.com
- * Version: 0.4
+ * Version: 0.4.1
  * Text Domain: dingsbeerblog
  * Author: Nathan Dias
  * Author URI: https://nathandias.com
