b512af9174e418b8dcc6119b43e8db43b2e93947
Nathan Dias <nathan.dias@gmail.com>
2021-07-02 12:02:24 -0700

    add codeception tests

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ rm dbb_plugin.zip yoko-child.zip Divi-child.zip csv_data.zip
 
 # zip up the plugin
 cp ./README.md ./TODO.md dingsbeer
-zip -x "dingsbeer/test" -x "dingsbeer/test/*" -x "dingsbeer/.gitignore" -r dbb_plugin.zip dingsbeer
+zip -x "dingsbeer/test" -x "dingsbeer/test/*" -x "dingsbeer/tests" -x "dingsbeer/tests/*" -x "dingsbeer/vendor" -x "dingsbeer/vendor/*" -x "dingsbeer/.gitignore" -r dbb_plugin.zip dingsbeer
 rm dingsbeer/README.md dingsbeer/TODO.md
 
 # zip up the yoko-child theme
