08c8d65cd42db076f0c715c2a57ffb3ae57ac7e5
Nathan Dias <nathan.dias@gmail.com>
2021-07-03 19:38:29 -0700

    add plugin version to zip archives

--- package.sh
+++ package.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+PACKAGE_VERSION=`grep "Version" dingsbeer/plugin.php | sed 's/^ \* Version: //'`
 
+echo "Creating package for version = ${PACKAGE_VERSION}"
+
+echo "*** packaging main plugin files ***"
 # remove old files
-rm dbb_plugin.zip yoko-child.zip Divi-child.zip csv_data.zip
+rm dbb_plugin*.zip yoko-child*.zip Divi-child*.zip csv_data*.zip
 
 # zip up the plugin
 cp ./README.md ./TODO.md dingsbeer
-zip -x "dingsbeer/test" -x "dingsbeer/test/*" -x "dingsbeer/tests" -x "dingsbeer/tests/*" -x "dingsbeer/vendor" -x "dingsbeer/vendor/*" -x "dingsbeer/.gitignore" -r dbb_plugin.zip dingsbeer
+zip -x "dingsbeer/test" -x "dingsbeer/test/*" -x "dingsbeer/tests" -x "dingsbeer/tests/*" -x "dingsbeer/vendor" -x "dingsbeer/vendor/*" -x "dingsbeer/.gitignore" -r "dbb_plugin.${PACKAGE_VERSION}.zip" dingsbeer
 rm dingsbeer/README.md dingsbeer/TODO.md
 
+echo "*** packaging yoko child theme files ***"
 # zip up the yoko-child theme
-zip -r yoko-child.zip yoko-child
+zip -r "yoko-child.${PACKAGE_VERSION}.zip" yoko-child
 
+echo "*** packaging Divi child theme files ***"
 # zip up the divi-child theme
-zip -r Divi-child.zip Divi-child
+zip -r "Divi-child.${PACKAGE_VERSION}.zip" Divi-child
 
+echo "*** packaging CSV testcases ***"
 # zip up the test data
 cd dingsbeer/test/
-zip csv_data.zip *
+zip "csv_data.${PACKAGE_VERSION}.zip" *
 cd ../..
-mv dingsbeer/test/csv_data.zip .
+mv dingsbeer/test/csv_data.${PACKAGE_VERSION}.zip .
 
